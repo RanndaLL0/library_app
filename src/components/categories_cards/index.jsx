c6082e06b3ecb7e7902e5eb7fb372b775355197b
RanndaLL0 <felipevictor5120@gmail.com>
2025-05-16 21:04:29 -0300

    feat: subindo o card principal da home

---
+++ src/components/categories_cards/index.jsx
@@ -0,0 +1,15 @@
+import { Pressable, StyleSheet, Text } from "react-native";
+
+export default function CateriesCards() {
+    return (
+        <Pressable>
+            <Text>Categoria</Text>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        backgroundColor: "#202020"
+    }
+})
